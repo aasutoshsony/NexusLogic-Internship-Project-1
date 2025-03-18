3ace30e51bd0312f2fa0672305e39df6b2c35e12
Aasutosh Kumar Sony <21053257@kiit.ac.in>
2025-03-18 15:37:02 +0530

    mar commit

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -7,7 +7,7 @@ const FoodDisplay = (category) => {
     const {food_list} = useContext (StoreContext)
   return (
     <div className='food-display' id='food-display'>
-      <h2>Explore our all Jewelleries Items</h2>
+      <h2>Explore our all Jewelleries Items!</h2>
       {/* <h2>Top dishes near you</h2> */}
       <div className="food-display-list">
         {food_list.map ((item,index)=>{
